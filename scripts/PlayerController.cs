515d70f413f82767da9ac38a12cd61aa3a7858fa
Tharle Josefi <tharlejc@gmail.com>
2026-03-23 16:51:39 -0400

    add player movement and add camera rotation (not working tho)

---
+++ scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+using Godot;
+using System;
+
+public partial class PlayerController : CharacterBody3D
+{
+	public float Speed {get; set;} = 5.0f;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector3 velocity = Velocity;
+
+		// Add the gravity.
+		// if (!IsOnFloor())
+		// {
+		// 	velocity += GetGravity() * (float)delta;
+		// }
+
+		// Get the input direction and handle the movement/deceleration.
+		Vector2 inputDir = Input.GetVector("move_right" , "move_left", "move_back", "move_forward");
+		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+		if (direction == Vector3.Zero)
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+		} else
+		{
+			velocity.X = direction.X * Speed;
+			velocity.Z = direction.Z * Speed;
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
--- scripts/camera/CameraController.cs
+++ scripts/camera/CameraController.cs
@@ -1,17 +1,73 @@
 using Godot;
 using System;
+using System.Diagnostics;
 
 public partial class CameraController : Camera3D
 {
 	[Export] Node3D toFollow;
+	public float Speed {get; set;} = 3.0f;
+	public float rotationSpeed = 15.0f; // angle per second
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
+	private Vector3 _deltaTargetDistance;
+
+	public Vector3 Forward{get
+		{
+			return -GlobalTransform.Basis.Z.Normalized();
+		}
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Ready()
+    {
+        _deltaTargetDistance = Position - toFollow.Position;
+    }
+
+
 	public override void _Process(double delta)
 	{
+        ToFollowTarget(delta);
+		RotateCamera(delta);
+
 	}
+
+
+    private void ToFollowTarget(double delta)
+    {
+        Vector3 dirToTarget = (toFollow.Position - Position + _deltaTargetDistance).Normalized();
+		dirToTarget.Y = 0; // ignore height
+
+		if(dirToTarget == Vector3.Zero) return;
+
+		float speedPerFrame = Speed * (float) delta;
+
+		Position += dirToTarget * speedPerFrame;
+    }
+
+	private void RotateCamera(double delta)
+    {
+		float inputRotationDir = Input.GetAxis("camera_rotate_right" , "camera_rotate_left");
+		GD.Print($"ROTATE CAMERA: Dir -> {inputRotationDir}");
+		if(inputRotationDir == 0) return;
+
+        Vector3 dirToTarget = (toFollow.Position - Position).Normalized();
+		float angle = dirToTarget.AngleTo(Forward);
+		GD.Print($"ANGLE -> {angle}");
+		float rotationSpeedDelta = rotationSpeed * (float) delta;
+		
+		angle += inputRotationDir * rotationSpeedDelta;
+
+    }
+
+	public override void _Draw()
+    {
+        // Your draw commands here.
+    }
+
+
+	private void DrawForward()
+	{
+		Vector3 forward  = Forward;
+		Vector3 pointForward = Position + (Forward * 5.0f);
+		Debug.DrawLine(Position, pointForward, Colors.Green, 2.0f);
+	}
+
 }
